8a22f84913bacbde837a318ab0ba47e4dce49ea9
bricefriha <contact@bricefriha.dev>
2024-07-03 23:05:36 +0100

    Draw board

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-console.log("Hello, World!");
+// Draw the board
+//
+// Draw the row slots
+let emptyRow = "";
+for (let i = 0; i < 16; ++i)
+    emptyRow += ' ';
+// Draw the rows
+for (let i = 0; i < 10; ++i)
+    console.log(emptyRow);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+
+// Draw the board
+//
+// Draw the row slots
+let emptyRow: string = "";
+for (let i: number = 0; i < 16; ++i)
+    emptyRow += ' ';
+// Draw the rows
+for (let i: number= 0; i < 10; ++i)
+    console.log(emptyRow);
