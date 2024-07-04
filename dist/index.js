3abe691ba563099c856ede1214813179a2e2c3f9
bricefriha <contact@bricefriha.dev>
2024-07-04 13:52:29 +0100

    Manage collisions

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,14 @@
 const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let positions = ["A:0", "A:1"];
+let positions = [createPiece("A:0"), createPiece("A:1")];
+// add ground
+for (let i = 0; i <= 10; i++) {
+    positions.push(createPiece(`P:${i}`, true));
+}
 let groundLvl = 16;
 // Draw the board
 //
 // Refresh
-setInterval(() => renderBoard(true), 2000);
+setInterval(() => renderBoard(true), 500);
 /**
  * Render the board
  * @param isRefreshing render the board on refresh
@@ -14,10 +18,12 @@ function renderBoard(isRefreshing = false) {
     console.log('\u001Bc\u001B[3J');
     if (isRefreshing)
         for (let posX = 0; posX < positions.length; posX++) {
-            const position = positions[posX];
-            const pos = position.split(':');
-            if (alpha.indexOf(pos[0]) <= groundLvl) {
-                positions.splice(posX, 1, `${alpha[alpha.indexOf(pos[0]) + 1]}:${pos[1]}`);
+            if (!positions[posX].frozen) {
+                const position = positions[posX].coordinate;
+                const pos = position.split(':');
+                if (alpha.indexOf(pos[0]) <= groundLvl) {
+                    positions.splice(posX, 1, createPiece(`${alpha[alpha.indexOf(pos[0]) + 1]}:${pos[1]}`));
+                }
             }
         }
     // Draw the rows
@@ -25,32 +31,60 @@ function renderBoard(isRefreshing = false) {
         let row = "";
         const alphaY = alpha[iy];
         // get position of the row
-        const slots = positions.filter(p => p.includes(alphaY));
+        const slots = positions.filter(p => p.coordinate.includes(alphaY));
         // Let see if there is any item on the row
         if ((slots === null || slots === void 0 ? void 0 : slots.length) > 0) {
-            for (let ix = 0; ix < 10; ix++) {
+            for (let ix = 0; ix <= 10; ix++) {
+                const slot = slots.find(e => e.coordinate.includes(`${ix}`));
                 // The read slot if occupied
-                if (slots.find(e => e.includes(`${ix}`)))
-                    row += '🟩';
+                if (slots.find(e => e.coordinate.includes(`${ix}`)))
+                    row += slot.symbol;
                 else
-                    row += ' ';
+                    row += '  ';
             }
+            // Draw the row
             console.log(row);
+            // list the pieces bellow
+            const bellowPieces = [];
+            positions.forEach(p => {
+                const y = p.coordinate.split(':')[0];
+                if (!p.frozen) {
+                    const c = positions.find(ps => ps.coordinate === `${alpha[alpha.indexOf(y) + 1]}:${p.coordinate.split(':')[1]}`);
+                    if (c)
+                        bellowPieces.push(c);
+                    //     const y = p.coordinate.split(':')[0]
+                    //     bellowPieces.push(createPiece(`${alpha[alpha.indexOf(y) + 1]}:${p.coordinate.split(':')[1]}`));
+                }
+            });
+            const collisions = bellowPieces.filter(bp => bp.frozen);
+            const collided = collisions.length > 0;
             // Check if a piece hit the floor
-            if (iy === groundLvl) {
-                AddNewPieces(["A:0", "A:1"]);
+            if (collided) {
+                positions.forEach(p => p.frozen = true);
+                //console.log(positions);
+                AddNewPieces(["A:1", "A:2", "A:3", "B:3"]);
                 --groundLvl;
             }
         }
         else
             console.log("          ");
     }
-    console.warn("⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛");
-    console.log(positions);
 }
 function AddNewPieces(newPieces) {
-    console.log(newPieces);
     for (let index = 0; index < newPieces.length; index++)
-        positions.push(newPieces[index]);
+        positions.push(createPiece(newPieces[index]));
+}
+function createPiece(coord, isProp = false) {
+    if (isProp)
+        return {
+            coordinate: coord,
+            symbol: '⬛',
+            frozen: true
+        };
+    return {
+        coordinate: coord,
+        symbol: '🟩',
+        frozen: false
+    };
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 
 const alpha : string[] =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let positions: string[] = ["A:0", "A:1"];
+
+interface Piece {
+    coordinate: string,
+    symbol: string,
+    frozen: boolean
+}
+let positions: Piece[] = [createPiece("A:0"), createPiece("A:1")];
+
+// add ground
+for (let i = 0; i <= 10; i++) {
+    positions.push(createPiece(`P:${i}`, true));
+}
 let groundLvl: number = 16;
 // Draw the board
 //
 
 
 // Refresh
-setInterval(() => renderBoard(true), 2000);
+setInterval(() => renderBoard(true), 500);
 /**
  * Render the board 
  * @param isRefreshing render the board on refresh
@@ -18,10 +29,12 @@ function renderBoard(isRefreshing : boolean = false) {
 
     if (isRefreshing)
         for (let posX = 0; posX < positions.length; posX++) {
-            const position = positions[posX];
-            const pos = position.split(':');
-            if (alpha.indexOf(pos[0]) <= groundLvl) {
-                positions.splice(posX, 1, `${alpha[alpha.indexOf(pos[0]) + 1]}:${pos[1]}`);
+            if (!positions[posX].frozen) {
+                const position = positions[posX].coordinate;
+                const pos = position.split(':');
+                if (alpha.indexOf(pos[0]) <= groundLvl) {
+                    positions.splice(posX, 1, createPiece(`${alpha[alpha.indexOf(pos[0]) + 1]}:${pos[1]}`));
+                }
             }
         }
     
@@ -30,34 +43,66 @@ function renderBoard(isRefreshing : boolean = false) {
         let row: string = "";
         const alphaY = alpha[iy];
         // get position of the row
-        const slots = positions.filter(p => p.includes(alphaY));
+        const slots = positions.filter(p => p.coordinate.includes(alphaY));
         // Let see if there is any item on the row
         if (slots?.length > 0) {
-            for (let ix: number = 0; ix < 10; ix++){
+            for (let ix: number = 0; ix <= 10; ix++){
+                const slot = slots.find(e => e.coordinate.includes(`${ix}`))
                 // The read slot if occupied
-                if (slots.find(e => e.includes(`${ix}`)))
-                    row += '🟩';
+                if (slots.find(e => e.coordinate.includes(`${ix}`)))
+                    row += slot.symbol;
                 else
-                    row += ' ';
+                    row += '  ';
 
             }
 
+            // Draw the row
             console.log(row);
-        
+            
+            // list the pieces bellow
+            const bellowPieces: Piece[] = [];
+            
+            positions.forEach(p => {
+                const y = p.coordinate.split(':')[0]
+                if (!p.frozen) {
+                    const c = positions.find(ps => ps.coordinate ===`${alpha[alpha.indexOf(y) + 1]}:${p.coordinate.split(':')[1]}`)
+                    if (c)
+                        bellowPieces.push(c);
+                //     const y = p.coordinate.split(':')[0]
+                //     bellowPieces.push(createPiece(`${alpha[alpha.indexOf(y) + 1]}:${p.coordinate.split(':')[1]}`));
+                }
+            });
+            const collisions = bellowPieces.filter(bp => bp.frozen);
+            const collided = collisions.length >0;
             // Check if a piece hit the floor
-            if (iy === groundLvl) {
-                AddNewPieces(["A:0", "A:1"]);
+            if (collided) {
+                positions.forEach(p => p.frozen = true);
+                //console.log(positions);
+                AddNewPieces(["A:1", "A:2", "A:3", "B:3"]);
                 --groundLvl;
             }
         }
         else
             console.log("          ");
     }
-    console.warn("⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛");
-    console.log(positions);
 }
 
 function AddNewPieces(newPieces: string[]) {
     for (let index = 0; index < newPieces.length; index++)
-        positions.push(newPieces[index]);
+        positions.push(createPiece(newPieces[index]));
+}
+
+function createPiece(coord: string, isProp : boolean = false): Piece{
+    if (isProp)
+        return {
+            coordinate: coord,
+            symbol: '⬛',
+            frozen: true
+        };
+    
+    return {
+        coordinate: coord,
+        symbol: '🟩',
+        frozen: false
+    };
 }
